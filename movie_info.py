b658281f5708dcd95e4682527de04accf41a89e0
Olga_S <Oleandora@gmail.com>
2018-04-20 22:50:48 +0300

    Extracted group of parsing functions

--- movie_info.py
+++ movie_info.py
@@ -1,36 +1,5 @@
-import requests, ast
-from bs4 import BeautifulSoup
+from parsing import *
 from database import Movie, session
-
-
-def extract_title(html):
-    raw_title = html.find(attrs={'class': 'originalTitle'}).text
-    return raw_title.replace('(original title)', '').strip()
-
-
-def extract_poster(html):
-    return html.find(attrs={'class': 'poster'}).find('img')['src']
-
-
-def extract_text_with_itemprop(html, itemprop):
-    raw_data = html.findAll(itemprop=itemprop)
-    return [data.text.strip().replace(',', '') for data in raw_data]
-
-
-def extract_actors(html):
-    return extract_text_with_itemprop(html, 'actors')
-
-
-def extract_directors(html):
-    return extract_text_with_itemprop(html, 'director')
-
-
-def extract_genres(html):
-    return extract_text_with_itemprop(html, 'genre')[:-1] # last element dublicate data in inappropriate way, so I cut off it
-
-
-def extract_rating(html):
-    return html.find(itemprop='ratingValue').text
 
 
 def collect_data(html, imdb_id='0'):
@@ -44,11 +13,6 @@ def collect_data(html, imdb_id='0'):
         'directors': extract_directors(html),
         'comments': '',
     }
-
-
-def get_html(url, title=''):
-    response = requests.get(url, params={'q': title, 'as_sitesearch': 'imdb.com'})
-    return BeautifulSoup(response.content, 'html.parser')
 
 
 def find_imdb_content(html):
@@ -70,6 +34,11 @@ def get_movie_data(title):
         return prepare_record(movie)
     except Exception as e:
         return 'Error: ', e.args[0]
+
+
+def str_to_list(data: str()):
+    data = list(data.split(', '))
+    return data
 
 
 def list_to_str(data: list):
@@ -97,11 +66,6 @@ def save_movie_info(movie):
 
 def saved_movies():
     return session.query(Movie).all()
-
-
-def str_to_list(data: str()):
-    data = list(data.split(', '))
-    return data
 
 
 def search_movie_data(title):
